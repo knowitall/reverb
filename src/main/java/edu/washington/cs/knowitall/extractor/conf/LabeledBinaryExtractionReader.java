ce25484594429545c33daaaf2cd8437a27ad59fd
Michael Schmitz <michael@schmitztech.com>
2011-06-09 18:04:06 +0000

    Reverse merged the L abeledBinaryExtractionReader to r1044.

--- src/main/java/edu/washington/cs/knowitall/extractor/conf/LabeledBinaryExtractionReader.java
+++ src/main/java/edu/washington/cs/knowitall/extractor/conf/LabeledBinaryExtractionReader.java
@@ -79,7 +79,7 @@ public class LabeledBinaryExtractionReader {
 			String[] startLen = line.split(" ");
 			int start = Integer.parseInt(startLen[0]);
 			int length = Integer.parseInt(startLen[1]);
-			return Range.fromInterval(start, length);
+			return new Range(start, length);
 		}
 		
 		private String[] readNextLines() throws IOException {
