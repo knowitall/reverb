8028c61f612c047cddc0c35135f0762fd5b8be72
Anthony Fader <afader@gmail.com>
2011-12-21 17:25:46 -0500

    Importing ConfidenceFunction.

---
+++ src/main/java/edu/washington/cs/knowitall/extractor/conf/ConfidenceFunction.java
@@ -0,0 +1,14 @@
+package edu.washington.cs.knowitall.extractor.conf;
+
+import edu.washington.cs.knowitall.nlp.extraction.ChunkedBinaryExtraction;
+
+/***
+ * Represents a confidence function for @{ChunkedBinaryExtraction} objects.
+ * @author afader
+ *
+ */
+public interface ConfidenceFunction {
+    
+    public double getConf(ChunkedBinaryExtraction extr) throws ConfidenceFunctionException;
+
+}
